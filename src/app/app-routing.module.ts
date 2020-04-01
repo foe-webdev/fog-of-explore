389bd02e1b603538f7759bc39e16838cc5abfba4
Liam Clarke <liam.clarke.94@outlook.com>
2020-04-01 13:30:31 +0100

    Add Explore Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'explore',
+        loadChildren: () => import('./pages/explore/explore.module').then(m => m.ExploreModule)
+    },
+    { path: '**', redirectTo: 'explore' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
