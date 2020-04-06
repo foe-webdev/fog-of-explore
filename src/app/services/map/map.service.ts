ba6fd61be10ff8bed60cf721a9850a3c70a07808
Liam Clarke <liam.clarke.94@outlook.com>
2020-04-06 13:41:11 +0100

    Add Discovery pack layer

--- src/app/services/map/map.service.ts
+++ src/app/services/map/map.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class MapService {
 
-    constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
-
+    getSelectedPackages(): Observable<any> {
+        return this.http.get('http://localhost:3000/packages').pipe(
+            map(data => {
+                const features: any[] = [];
+                for (const [_, feature] of Object.entries(data)) {
+                    features.push({
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: feature.coordinates
+                        },
+                        properties: {
+                            id: feature.id,
+                            title: feature.title,
+                            description: feature.description,
+                            icon: feature.icon,
+                        }
+                    });
+                }
+                return features;
+            })
+        );
+    }
 }
