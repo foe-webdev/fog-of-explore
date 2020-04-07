d755fb522711a38a8ec130fc63dbe1af0a8b3f34
Liam Clarke <liam.clarke.94@outlook.com>
2020-04-07 17:24:52 +0100

    Add mobile tabs component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+    constructor(
+        public platform: Platform
+    ) { }
+
+    isMobile(): boolean {
+        return this.platform.ANDROID || this.platform.IOS;
+    }
+}
